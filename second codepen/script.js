fef8d3a176f20fa8df1fcbd3c84893d70710d8ea
SzaSziPa <sz.szabina.kmne@gmail.com>
2020-04-14 15:48:09 +0200

    hibas

--- second codepen/script.js
+++ second codepen/script.js
@@ -1,12 +1,19 @@
-let color = 'purple';
-let number = "10";
+let color = "purple";
+let number = 10;
 let word = "cool";
 
-if (color === 'purple') {
-    $('#red').css('background', 'purple');
+if (color === "purple") {
+  $(".north").css("background", "purple");
 }
-if (number > "100") {
-    $('#yellow').text("whoah, that's a big number");
+
+if (number > 100) {
+  $(".east").text("whoah, that's a big number.");
 } else {
-    $('#yellow').text("just a regular number, please");
+  $(".east").text("just a regular number, please.");
+}
+
+if (word === "cool") {
+  $(".south").text("Power of DOM");
+} else {
+  $(".west").text("Power of DOM");
 }
