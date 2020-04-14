e40bbdd41229bd4b5d0d01b566c9447b6cb56175
SzaSziPa <sz.szabina.kmne@gmail.com>
2020-04-14 15:58:16 +0200

    Revert "hibas"
    This reverts commit fef8d3a176f20fa8df1fcbd3c84893d70710d8ea.

--- second codepen/script.js
+++ second codepen/script.js
@@ -1,19 +1,12 @@
-let color = "purple";
-let number = 10;
+let color = 'purple';
+let number = "10";
 let word = "cool";
 
-if (color === "purple") {
-  $(".north").css("background", "purple");
+if (color === 'purple') {
+    $('#red').css('background', 'purple');
 }
-
-if (number > 100) {
-  $(".east").text("whoah, that's a big number.");
+if (number > "100") {
+    $('#yellow').text("whoah, that's a big number");
 } else {
-  $(".east").text("just a regular number, please.");
-}
-
-if (word === "cool") {
-  $(".south").text("Power of DOM");
-} else {
-  $(".west").text("Power of DOM");
+    $('#yellow').text("just a regular number, please");
 }
