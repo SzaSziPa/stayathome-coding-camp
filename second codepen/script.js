aa2a4357f8c31075627df500712022380fdbfd22
SzaSziPa <sz.szabina.kmne@gmail.com>
2020-04-14 15:44:37 +0200

    sajat

---
+++ second codepen/script.js
@@ -0,0 +1,12 @@
+let color = 'purple';
+let number = "10";
+let word = "cool";
+
+if (color === 'purple') {
+    $('#red').css('background', 'purple');
+}
+if (number > "100") {
+    $('#yellow').text("whoah, that's a big number");
+} else {
+    $('#yellow').text("just a regular number, please");
+}
