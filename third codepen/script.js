6c0327b4e3c73db22d8def6786256e643cbafb59
SzaSziPa <sz.szabina.kmne@gmail.com>
2020-04-14 18:25:56 +0200

    waaaa1

---
+++ third codepen/script.js
@@ -0,0 +1,7 @@
+let names = ['Stepan', 'Noemi', 'Nimrod', 'Amalia', 'Sabina'];
+let felsorolas = $('.ul').first();
+console.log(felsorolas)
+names.forEach(element => {
+    console.log(element)
+});
+
