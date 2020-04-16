acc2c054dae218ffdb311cff7bf2a8753429e66e
SzaSziPa <sz.szabina.kmne@gmail.com>
2020-04-16 16:12:46 +0200

    siker

--- 4codepen/script.js
+++ 4codepen/script.js
@@ -1,7 +1,15 @@
+let count = 0;
+
+
 $("button").on('click', (event) => {
     console.log("Yeah, you clicked me");
   });
 
 $("#gomb").click(() => {
     $("#gomb").text("Liked!")
+});
+
+$("#gomb").click(() => {
+  count += 1;
+  $("#like").text(`${count}`);
 });
